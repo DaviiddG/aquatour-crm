abffa50c952212f9dfc1a314310350802096e5dc
Oscar <oscar@example.com>
2025-09-22 14:46:47 -0500

    fix: actualizar script para funcionar sin sudo

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Instalar dependencias del sistema
-echo "Actualizando el sistema..."
-sudo apt-get update
-echo "Instalando dependencias..."
-sudo apt-get install -y curl unzip xz-utils
-
-# Descargar y configurar Flutter
-echo "Descargando Flutter..."
-curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.1-stable.tar.xz | tar xJ
+# Configurar el PATH de Flutter
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# Verificar si Flutter ya está descargado
+if [ ! -d "flutter" ]; then
+    echo "Descargando Flutter..."
+    curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.1-stable.tar.xz | tar xJ
+fi
+
+# Configurar Git para evitar el error de propiedad
+git config --global --add safe.directory /vercel/path0/flutter
 
 # Verificar la instalación
 echo "Verificando la instalación de Flutter..."
